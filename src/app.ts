03b2a4d2b8d6d4189f50efbe2a3e2aad38cb4a80
amin foroutan <aminforoutan7@gmail.com>
2021-07-15 02:57:50 +0430

    added ErrorHandlerMiddleWare and updated middleware import

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express, { Application, Request, Response } from 'express'
 import { black } from 'chalk'
 import config from './config'
-import logger, { ReqLoggerMiddleWare } from './logger'
+import logger from './logger'
+import { ReqLoggerMiddleWare, ErrorHandlerMiddleWare } from './middlewares'
+
 const app: Application = express()
 
 const port = config.port as number
 const host = config.host as string
 app.use(ReqLoggerMiddleWare)
-app.get('/', (req: Request, res: Response): void => {
-  res.send('hello')
-})
 
+// Error Handling
+app.use(ErrorHandlerMiddleWare)
 app.listen(port, host, () => {
   logger.info(`Server is Running on Port: [${black.bgGreen(port)}] and Host: [${black.bgYellow('http://' + host)}]`)
 })
