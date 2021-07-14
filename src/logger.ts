02b65d06d1908fde93456c9e05b163adbc1cab15
amin foroutan <aminforoutan7@gmail.com>
2021-07-15 01:43:28 +0430

    created ReqLoggerMiddleWare

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,19 @@
 import pino from 'pino'
 import dayjs from 'dayjs'
+import { Response, Request, NextFunction } from 'express'
+import { yellow, green, magenta } from 'chalk'
 
-export default pino({
+const Logger = pino({
   prettyPrint: true,
   base: {
     pid: false
   },
   timestamp: () => `,"time":"${dayjs().format()}"`
 })
+
+export const ReqLoggerMiddleWare = (req: Request, res: Response, next: NextFunction): void => {
+  Logger.info(`[${green(req.method)}] Request from IP:[${yellow(req.socket.remoteAddress)}] for Endpoint:[${magenta(req.url)}]`)
+  return next()
+}
+
+export default Logger
