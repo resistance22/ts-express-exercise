34082c0819284a9b6bcc316d01cf805c7f3b2d8d
amin foroutan <aminforoutan7@gmail.com>
2021-07-15 01:43:59 +0430

    added ReqLoggerMiddleWare to app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import { black } from 'chalk'
 import config from './config'
-const app = express()
+import logger, { ReqLoggerMiddleWare } from './logger'
+const app: Application = express()
 
 const port = config.port as number
 const host = config.host as string
-
+app.use(ReqLoggerMiddleWare)
 app.get('/', (req: Request, res: Response): void => {
   res.send('hello')
 })
 
 app.listen(port, host, () => {
-  console.log(`Server is Running on Port: [${black.bgGreen(port)}] and Host: [${black.bgYellow('http://' + host)}]`)
+  logger.info(`Server is Running on Port: [${black.bgGreen(port)}] and Host: [${black.bgYellow('http://' + host)}]`)
 })
