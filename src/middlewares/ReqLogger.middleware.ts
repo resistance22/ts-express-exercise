1ac43ca22ccec83ec293fc7ce8a4ead2563d2412
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-15 12:24:09 +0430

    white space!

--- src/middlewares/ReqLogger.middleware.ts
+++ src/middlewares/ReqLogger.middleware.ts
@@ -4,7 +4,6 @@ import { yellow, green, black } from 'chalk'
 import { Middleware } from './interfaces'
 
 class ReqLoggerMiddleWare implements Middleware{  
-  
   middleware(req: Request, res: Response, next: NextFunction): void {
     Logger.info(`[${green(req.method)}] Request from IP:[${yellow(req.socket.remoteAddress)}] for Endpoint:[${black.bgGreen(req.url)}]`)
     return next()
