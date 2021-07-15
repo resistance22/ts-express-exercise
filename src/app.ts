3705e0efad1e363244f587c91c7f4c3305999cd0
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-15 16:54:43 +0430

    setup database

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import { black } from 'chalk'
 import config from './config'
 import logger from './logger'
 import { ReqLoggerMiddleWare, ErrorHandlerMiddleWare } from './middlewares'
 import { applyMiddleware } from './utils'
+import DB from './DB'
 
 const app: Application = express()
 const port = config.port as number
 const host = config.host as string
-
+const db = new DB()
 // Reuquest Logger
 applyMiddleware(app, new ReqLoggerMiddleWare())
 
@@ -16,4 +17,5 @@ applyMiddleware(app, new ReqLoggerMiddleWare())
 app.use(ErrorHandlerMiddleWare)
 app.listen(port, host, () => {
   logger.info(`Server is Running on Port: [${black.bgGreen(port)}] and Host: [${black.bgYellow('http://' + host)}]`)
+  db.setUp()
 })
