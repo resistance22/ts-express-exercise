6c3367c80df1211b76b81035f7ca82c56400dcba
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-15 12:22:15 +0430

    created applyMiddleware function

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+import { Application } from "express"
+import { Middleware } from "./middlewares/interfaces"
+
+export const applyMiddleware = (app: Application, Middleware: Middleware): void => {
+  app.use(Middleware.middleware)
+}
