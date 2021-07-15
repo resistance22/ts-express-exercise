6cc8f7645f904cbb3a74c7e9ea370c3ddda0c5e3
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-15 16:54:30 +0430

    added dbURI to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ dotenv.config()
 
 export default {
   port: process.env.SERVER_PORT || 1337,
-  host: process.env.SERVER_HOST || 'localhost'
+  host: process.env.SERVER_HOST || 'localhost',
+  dbURI: process.env.DATABASE_URI
 }
