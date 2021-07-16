fdea037c47ba6f65a9ece3167e91dd94d2df675e
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-16 17:30:48 +0430

    no newline

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import { Application } from "express"
-import { Middleware } from "./middlewares/interfaces"
+import { Application } from 'express'
+import { Middleware } from './middlewares/interfaces'
 
 export const applyMiddleware = (app: Application, Middleware: Middleware): void => {
   app.use(Middleware.middleware)
