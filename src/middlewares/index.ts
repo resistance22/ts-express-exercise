e47328c6752d7a5672006649a118910da9a35ecc
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-16 17:31:24 +0430

    exported ReqBodyValidator middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { default as ReqLoggerMiddleWare } from './ReqLogger.middleware'
 export { default as ErrorHandlerMiddleWare } from './ErrorHandler.midlleware'
+export { default as ReqBodyValidatorMiddleWare } from './reqBodyValidator.middleware'
