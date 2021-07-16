558d5ccfa71efcc468bfefbbfaade6487bb9246f
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-17 02:55:53 +0430

    used MongooseMemoryServer for better test speed

--- src/Tests/TestUtils.ts
+++ src/Tests/TestUtils.ts
@@ -1,13 +1,18 @@
 import mongoose, { Model } from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+let Server: MongoMemoryServer
 
 export const dbConnect = async () => {
+  Server = await MongoMemoryServer.create()
   const mongooseOpts = {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
   }
-  await mongoose.connect('mongodb://localhost:27017/TEST_DB', mongooseOpts)
+  const uri = Server.getUri()
+  await mongoose.connect(uri, mongooseOpts)
 }
 
 export const deleteAllData = async (model: Model<any>) => {
@@ -21,4 +26,5 @@ export const syncAllIndexes = async (model: Model<any>) => {
 export const dropDB = async () => {
   await mongoose.connection.dropDatabase()
   await mongoose.connection.close()
+  await Server.stop()
 }
