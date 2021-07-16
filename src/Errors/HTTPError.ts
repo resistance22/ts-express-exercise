7bc5589297f583ab474c54ec0d8d4cde154438ed
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-16 17:27:25 +0430

    changed message to array of messages

--- src/Errors/HTTPError.ts
+++ src/Errors/HTTPError.ts
@@ -1,7 +1,12 @@
-export default class HTTPError extends Error {
-  constructor(public status: number, public message: string) {
-    super(message)
+import { CustomError } from './interfaces'
+
+class HttpError extends Error implements CustomError {
+  constructor(public status: number, public messages: string[]) {
+    super('Http Error accured!')
     this.name = 'HTTPError'
     this.status = status
+    this.messages = messages
   }
 }
+
+export default HttpError
