88465738cd5678456a69d8090fd488dc0242f5b3
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-16 17:28:31 +0430

    changed error type check from instanceof to name

--- src/middlewares/ErrorHandler.midlleware.ts
+++ src/middlewares/ErrorHandler.midlleware.ts
@@ -1,16 +1,11 @@
 import { Request, Response, Errback, NextFunction } from 'express'
 import Logger from '../logger'
-import HTTPError from '../Errors/HTTPError'
-
-interface CustomError extends Errback {
-  message: string
-  status: number
-}
+import { CustomError } from '../Errors/interfaces'
 
 const ErrorHandlerMiddleWare = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
   Logger.error(err)
-  if (err instanceof HTTPError) {
-    return res.status(err.status).json(err.message)
+  if (err.name === 'HTTPError') {
+    return res.status(err.status).json(err.messages)
   }
   return res.status(500).json('something went wrong!')
 }
