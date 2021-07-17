163634306cdbe6ce96063701ffc3a9f80f6ba416
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-17 15:07:36 +0430

    added createUser function

--- src/Controller/Register.controller.ts
+++ src/Controller/Register.controller.ts
@@ -1,16 +1,17 @@
 import { Controller } from './interfaces'
 import { UserDTO } from '../DTO'
 import { ReqBodyValidatorMiddleWare } from '../middlewares'
-
+import { createUser } from '../DBServices/User.service'
 class RegisterController extends Controller {
   constructor() {
     super('/register')
   }
   configure = () => {
     const ReqValidator = new ReqBodyValidatorMiddleWare(UserDTO)
-    this.Router.post(this.endpoint, ReqValidator.middleware, (req, res) => {
+    this.Router.post(this.endpoint, ReqValidator.middleware, async (req, res) => {
       const { body } = req
-      res.json(body)
+      const insertedUser = await createUser(body)
+      res.json(insertedUser)
     })
     return this.Router
   }
