8f8d22231e3c9971c22a119d6b37a11d5602bb25
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-17 14:35:34 +0430

    finished POST /register endpoint testing

--- Tests/api/Register.test.ts
+++ Tests/api/Register.test.ts
@@ -1,12 +1,12 @@
 import { expect } from 'chai'
-import { UserModel } from '../../Models'
+import { UserModel } from '../../src/Models'
 import Sinon, { SinonStub } from 'sinon'
-import app from '../../app'
+import app from '../../src/app'
 import chai from 'chai'
-import { dbConnect, dropDB } from '../TestUtils'
 
 describe('POST /register', function () {
   let stub: SinonStub
+  let agent = chai.request
 
   before('Create Stub', async () => {
     const fakeUser = {
@@ -26,13 +26,71 @@ describe('POST /register', function () {
   })
 
   it('Should should not work when data in malformed', () => {
-    chai
-      .request(app)
+    agent(app)
       .post('/register')
       .send({})
-      .then((res) => {
-        console.log(res)
-        expect(res).to.have.status(422)
+      .end((err, res) => {
+        if (!err) {
+          expect(res).to.have.status(422)
+          expect(res.body).to.be.an('array')
+        }
+      })
+  })
+
+  it('Should detect wrong email format', () => {
+    const data = {
+      password: '12345678',
+      firstName: 'Amin',
+      lastName: 'Foroutan',
+      email: 'amin23',
+      mobileNumber: '+989301112524'
+    }
+    agent(app)
+      .post('/register')
+      .send(data)
+      .end((err, res) => {
+        if (!err) {
+          expect(res).to.have.status(422)
+          expect(res.body).to.be.an('array')
+        }
+      })
+  })
+
+  it('Should detect wrong email mobileNumber', () => {
+    const data = {
+      password: '12345678',
+      firstName: 'Amin',
+      lastName: 'Foroutan',
+      email: 'amin23@gmail.com',
+      mobileNumber: '09301112524'
+    }
+    agent(app)
+      .post('/register')
+      .send(data)
+      .end((err, res) => {
+        if (!err) {
+          expect(res).to.have.status(422)
+          expect(res.body).to.be.an('array')
+        }
+      })
+  })
+
+  it('Should detect wrong password shorter than 8 charachters', () => {
+    const data = {
+      password: '1234567',
+      firstName: 'Amin',
+      lastName: 'Foroutan',
+      email: 'amin23@gmail.com',
+      mobileNumber: '+989301112524'
+    }
+    agent(app)
+      .post('/register')
+      .send(data)
+      .end((err, res) => {
+        if (!err) {
+          expect(res).to.have.status(422)
+          expect(res.body).to.be.an('array')
+        }
       })
   })
 })
