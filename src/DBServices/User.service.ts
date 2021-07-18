79a2c00587d1a2cdba577dd471091733c5f0dcf0
amin foroutan <aminforoutan7@gmail.com>
2021-07-18 21:42:25 +0430

    removed password omiting

--- src/DBServices/User.service.ts
+++ src/DBServices/User.service.ts
@@ -23,15 +23,7 @@ export interface UserReurnValue {
 export async function createUser(userObject: UserObject) {
   try {
     const savedUser = await UserModel.create(userObject)
-    return {
-      _id: savedUser.id,
-      firstName: savedUser.firstName,
-      lastName: savedUser.lastName,
-      email: savedUser.email,
-      mobileNumber: savedUser.mobileNumber,
-      createdAt: savedUser.createdAt.toISOString(),
-      updatedAt: savedUser.updatedAt.toISOString()
-    }
+    return savedUser
   } catch (e) {
     Logger.error(e)
     if (e.code === 11000) {
