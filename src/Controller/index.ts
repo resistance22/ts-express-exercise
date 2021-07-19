c6e2e5a1e7345d11ac49d983fb0c54ca48b2b173
amin foroutan <aminforoutan7@gmail.com>
2021-07-19 15:59:49 +0430

    style: moved globalRouter to the top of file

--- src/Controller/index.ts
+++ src/Controller/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 import RegisterController from './Register.controller'
 
-const registerRouter = new RegisterController().configure()
-
 const globalRouter = Router()
+const registerRouter = new RegisterController().configure()
 
 globalRouter.use('/', registerRouter)
 
