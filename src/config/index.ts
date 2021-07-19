dd346e4a609cb1fc3bf542f334c5c90c2c4c2b0c
amin foroutan <aminforoutan7@gmail.com>
2021-07-20 00:45:50 +0430

    feat: added secretToken to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ dotenv.config()
 export default {
   port: process.env.SERVER_PORT || 1337,
   host: process.env.SERVER_HOST || 'localhost',
+  tokenSecret: process.env.TOKEN_SECRET,
   dbURI: process.env.DATABASE_URI
 }
