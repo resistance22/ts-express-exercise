13b0c98aaa8d38e3007245a432cdfc0fce098f07
amin foroutan <aminforoutan7@gmail.com>
2021-07-20 03:17:23 +0430

    feat: added /login route controller

--- src/Controller/index.ts
+++ src/Controller/index.ts
@@ -1,8 +1,11 @@
 import RegisterController from './Register.controller'
+import LoginController from './Login.controller'
 import GlobalRouter from './GlobalRouter'
 
 const registerController = new RegisterController()
+const loginController = new LoginController()
 
 GlobalRouter.addRoute('/register', registerController)
+GlobalRouter.addRoute('/login', loginController)
 
 export default GlobalRouter
