ad32fd3d8f3bac187505d9cedc74f12659fb7e67
amin foroutan <aminforoutan7@gmail.com>
2021-07-19 15:56:53 +0430

    reafactor:used ommit function for cleaner code

--- src/Controller/Register.controller.ts
+++ src/Controller/Register.controller.ts
@@ -2,6 +2,8 @@ import { Controller } from './interfaces'
 import { UserDTO } from '../DTO'
 import { ReqBodyValidatorMiddleWare } from '../middlewares'
 import { createUser } from '../DBServices/User.service'
+import { omit } from '../utils'
+
 class RegisterController extends Controller {
   constructor() {
     super('/register')
@@ -11,7 +13,8 @@ class RegisterController extends Controller {
     this.Router.post(this.endpoint, ReqValidator.middleware, async (req, res) => {
       const { body } = req
       const insertedUser = await createUser(body)
-      res.json(insertedUser)
+      const sanitizedVersion = omit(insertedUser.toObject(), '__v', 'password')
+      res.json(sanitizedVersion)
     })
     return this.Router
   }
