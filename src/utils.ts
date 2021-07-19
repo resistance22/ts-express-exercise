05a436286c4000efb5cadd9543fe2beb609d8ca6
amin foroutan <aminforoutan7@gmail.com>
2021-07-19 15:49:50 +0430

    created the omit function

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,22 @@ import { Middleware } from './middlewares/interfaces'
 export const applyMiddleware = (app: Application, Middleware: Middleware): void => {
   app.use(Middleware.middleware)
 }
+
+interface Omit {
+  <T extends object, K extends [...(keyof T)[]]>(obj: T, ...keys: K): {
+    [K2 in Exclude<keyof T, K[number]>]: T[K2]
+  }
+}
+
+const omit: Omit = (obj, ...keys) => {
+  const ret = {} as {
+    [K in keyof typeof obj]: typeof obj[K]
+  }
+  let key: keyof typeof obj
+  for (key in obj) {
+    if (!keys.includes(key)) {
+      ret[key] = obj[key]
+    }
+  }
+  return ret
+}
