c4928bf46fbcb57a222b3e1ee3a02d9c1c285cd5
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-27 16:02:29 +0430

    feat: added test db uri based on NODE_ENV

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config()
-
+const env = process.env.NODE_ENV
 export default {
   port: process.env.SERVER_PORT || 1337,
   host: process.env.SERVER_HOST || 'localhost',
   tokenSecret: process.env.TOKEN_SECRET || 'verysecretkey',
-  dbURI: process.env.DATABASE_URI
+  dbURI: env === 'test' ? process.env.TEST_DATABASE_URI : process.env.DATABASE_URI
 }
