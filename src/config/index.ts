ba7ce5d9540d5b2aefa79fb78061d06caa171361
Amin Foroutan <aminforoutan7@gmail.com>
2021-07-27 17:12:03 +0430

    feat: added redis configuration
    connects to REDIS_TEST_DB if NODE_ENV is test othervise connects to REDIS_DB

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,7 @@ export default {
   port: process.env.SERVER_PORT || 1337,
   host: process.env.SERVER_HOST || 'localhost',
   tokenSecret: process.env.TOKEN_SECRET || 'verysecretkey',
-  dbURI: env === 'test' ? process.env.TEST_DATABASE_URI : process.env.DATABASE_URI
+  dbURI: env === 'test' ? process.env.TEST_DATABASE_URI : process.env.DATABASE_URI,
+  redisURI: process.env.REDIS_URI,
+  redisDB: env === 'test' ? process.env.REDIS_DB : process.env.REDIS_TEST_DB
 }
